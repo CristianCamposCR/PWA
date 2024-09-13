12cc1928c9fc9e7cf20a9012752e2ce19b95f23f
Cristian Campos <i20213tn003@utez.edu.mx>
2024-09-12 19:01:06 -0600

    [ADD] some changes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
+// Preguntamos si en navegador soporta el service worker y lo registra
 if(navigator.serviceWorker){
     
-    navigator.serviceWorker.register('./sw.js');
+    navigator.serviceWorker.register('../sw.js');
 }
 
